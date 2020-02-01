13ce4bbd3d1ae4910ee496e9eaca4f3644af8795
ogunb <babacanogun@gmail.com>
2020-02-01 21:31:12 +0300

    fix: card type constraints.

--- backend/db/cards.sql
+++ backend/db/cards.sql
@@ -1,7 +1,7 @@
 CREATE TABLE cards (
     SerialNumber UUID UNIQUE NOT NULL PRIMARY KEY,
     Name VARCHAR (50) NOT NULL,
-    Type VARCHAR (6) NOT NULL DEFAULT 'DARK' CHECK (Type in ('Monster', 'Trap', 'Spell')),
+    Type VARCHAR (7) NOT NULL DEFAULT 'Monster' CHECK (Type in ('Monster', 'Trap', 'Spell')),
     Attribute VARCHAR (6) NOT NULL DEFAULT 'DARK' CHECK (Attribute in ('DARK', 'DIVINE', 'EARTH', 'FIRE', 'LIGHT', 'WATER', 'WIND')),
     Level INT DEFAULT 1 NOT NULL CHECK (Level >= 1 AND Level <= 12),
     ImageUrl VARCHAR (1000),
