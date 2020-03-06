303b91bb3f19c8e9c60cee542108cae7ac4850f5
unknown <babacanogun@gmail.com>
2020-03-06 23:50:03 +0300

    feat: Implement find card use case.

---
+++ backend/src/use-cases/find-card.ts
@@ -0,0 +1,20 @@
+import { cardDb } from '../db'
+import { Card } from 'card-model'
+import { makeCard } from '../entities/index'
+
+export type FindCard = (arg: { serialNumber: string }) => Promise<Card>
+export default function makeFindCard (): FindCard {
+    return async function findCard ({ serialNumber }) {
+        if (serialNumber.length !== 36) {
+            throw new Error('Card serial number is not valid.')
+        }
+
+        const plainCard = await cardDb.findCard({ serialNumber })
+        if (!plainCard) {
+            throw new Error('Card does not exist.')
+        }
+
+        const card = makeCard(plainCard)
+        return card
+    }
+}
--- backend/src/use-cases/index.ts
+++ backend/src/use-cases/index.ts
@@ -1,5 +1,7 @@
 import makeCreateCard from './create-card'
 import makeFindCards from './find-cards'
+import makeFindCard from './find-card'
 
 export const createCard = makeCreateCard()
 export const findCards = makeFindCards()
+export const findCard = makeFindCard()
