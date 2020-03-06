c6b185b2db78e5241fc36edf42b0cb53f28e7d3c
unknown <babacanogun@gmail.com>
2020-03-07 02:07:16 +0300

    feat: Simplify user entity.

--- backend/src/entities/user.ts
+++ backend/src/entities/user.ts
@@ -7,27 +7,9 @@ type BuildMakeUserArguments = {
 export default function buildMakeUser ({ generateId }: BuildMakeUserArguments): () => User {
     return function makeUser (): User {
         const userId = generateId()
-        let cards: string[] = []
 
         return Object.freeze({
-            getUserId: () => userId,
-            getCards: () => cards,
-            addCard: (cardSerialNumber) => {
-                cards.push(cardSerialNumber)
-
-                return {
-                    newCardId: cardSerialNumber,
-                    cards
-                }
-            },
-            removeCard: (cardSerialNumber) => {
-                cards = cards.filter(cardId => cardId === cardSerialNumber)
-
-                return {
-                    removedCard: cardSerialNumber,
-                    cards
-                }
-            }
+            getUserId: () => userId
         })
     }
 }
--- backend/src/models/user-model.ts
+++ backend/src/models/user-model.ts
@@ -2,13 +2,4 @@ type CardIds = string[];
 
 export interface User {
     getUserId: () => string,
-    getCards: () => CardIds,
-    addCard: (cardSerialNumber: string) => {
-        newCardId: string,
-        cards: CardIds
-    },
-    removeCard: (cardSerialNumber: string) => {
-        removedCard: string,
-        cards: CardIds
-    },
 }
