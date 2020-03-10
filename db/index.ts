e7a1974ee2a5797b55978e93d8dcab225c879e46
unknown <babacanogun@gmail.com>
2020-03-10 18:29:37 +0300

    feat: Update DB connection and env sample file.

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,8 @@
+/* eslint-disable import/first */
+require('dotenv-flow').config()
+
 import path from 'path'
 import { makeDb } from '../src/db'
-require('dotenv-flow').config();
 
 (async function initializeDB () {
     const sql = makeDb()
--- src/db/index.ts
+++ src/db/index.ts
@@ -3,31 +3,9 @@ import makeCardDb from './cards-db'
 import makeUserDb from './user-db'
 
 export function makeDb () {
-    const {
-        DB_HOST,
-        DB_PORT,
-        DB_NAME,
-        DB_USERNAME,
-        DB_PASSWORD,
-        DATABASE_URL
-    } = process.env
+    const { DATABASE_URL } = process.env
 
-    if (DATABASE_URL) {
-        return postgres(DATABASE_URL, {
-            max: 10,
-            timeout: 60,
-            transform: {
-                column: postgres.toCamel
-            }
-        })
-    }
-
-    return postgres('localhost', {
-        host: DB_HOST,
-        port: DB_PORT,
-        database: DB_NAME,
-        username: DB_USERNAME,
-        password: DB_PASSWORD,
+    return postgres(DATABASE_URL, {
         max: 10,
         timeout: 60,
         transform: {
